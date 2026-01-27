abff317b3205b555e72d5cb03a5d6636e1eacf0c
Ginx07 <garanj@proton.me>
2026-01-27 11:14:37 -0600

    Project tiles fixed w/ new cursur and smooth glide

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Collapsible Work section
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -12,3 +13,54 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+// Draggable horizontal scroll for Projects section
+const slider = document.querySelector('.projects');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('dragging');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('dragging');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('dragging');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1.5;
+  slider.scrollLeft = scrollLeft - walk;
+});
+
+// Center project cards on fresh page
+window.addEventListener('load', () => {
+  const slider = document.querySelector('.projects');
+  const cards = slider.children;
+
+  const middleIndex = Math.floor(cards.length / 2);
+  const middleCard = cards[middleIndex];
+
+  const offset = middleCard.offsetLeft - (slider.clientWidth / 2) + (middleCard.clientWidth / 2);
+
+  slider.scrollLeft = offset;
+});
+
+slider.addEventListener('mousedown', (e) => {
+  if (!e.target.closest('.project-card')) return;
+  isDown = true; startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
